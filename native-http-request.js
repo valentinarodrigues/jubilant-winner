ededa80a645ce8b8c2a276ec3ae335986232f466
Valentina Rodrigues <valentina@heady.io>
2021-06-25 18:29:33 +0530

    raw http library

--- native-http-request.js
+++ native-http-request.js
@@ -1,8 +1,25 @@
 const http = require('http');
-const url = `http://api.weatherstack.com/current?access_key=86bb3660366c84d1d2a47f853bf040d1&units=f&query=${lattitude},${longitude}`
-http.request(url, (response) => {
+const url = `http://api.weatherstack.com/current?access_key=86bb3660366c84d1d2a47f853bf040d1&units=f&query=18.1,72.04`
+const request = http.request(url, (response) => {
+    let data = '';
     // callback can be registered on different events
-    response.on('data', (sample) => {
-        console.log(sample)
+    // handler is the function
+    // data is an event 
+    response.on('data', (chunk) => {
+        // Prints a buffer
+        // console.log(chunk)
+        data += chunk.toString()
+    })
+
+    response.on('end', () => {
+        const body = JSON.parse(data)
+        console.log('end')
+        console.log(body)
     })
 })
+
+request.on('error', (err) => {
+    console.log(err)
+})
+
+request.end()
